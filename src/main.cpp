54423a9a312340631acff98f2e755f0038dedf25
Janik Schumacher <rndmb0t@gmail.com>
2021-10-12 22:26:23 +0200

    more leds

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,15 @@ int main()
 {
     // Initialise the digital pin LED1 as an output
     DigitalOut led(LED1);
+    DigitalOut led2(LED2);
+    DigitalOut led3(LED3);
+    led2 = 1;
 
     while (true)
     {
         led = !led;
+        led2 = !led2;
+        led3 = !led3;
         thread_sleep_for(BLINKING_RATE_MS);
     }
 }
